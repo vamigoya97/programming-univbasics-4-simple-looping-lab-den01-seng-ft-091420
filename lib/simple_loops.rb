42172746af312e62d46f8fd06c8470e47352c33e
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-06 17:50:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,42 @@
 # Write your methods here
+
+
+def loop_message_five_times(string)
+  counter = 0 
+  while counter < 5 do
+    puts string
+    counter += 1 
+  end
+end
+
+def loop_message_n_times(string, n)
+  counter = 0 
+  while counter < n do
+    puts string
+    counter += 1 
+  end
+end
+
+def output_array(array)
+  index = 0 
+  while index =< array.length - 1 do
+    puts array[index]
+    index += 1 
+  end
+end
+
+def return_string_array(array)
+  index = 0 
+  while index =< array.length -1 do
+    array[index] = array[index].to_s
+    index += 1 
+  end
+  return array
+end
+
+    
+    
+    
+    
+    
+    
